c428fdd2fcd2601eab6af46e2ab6d396a7a75f1f
hemadhami001 <hemadhami365@gamil>
2024-05-27 13:49:01 +0545

    Add project files.

---
+++ Application/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,43 @@
+﻿using Application.Common.Interfaces;
+using Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Products.Commands.CreateProduct
+{
+    public class CreateProductCommand:IRequest<int>
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+    }
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, int>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        public CreateProductCommandHandler(IApplicationDbContext context)
+        {
+            this._dbContext = context;
+        }
+        public async Task<int> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var entity = new Product
+            {
+                ProductName = command.ProductName,
+                Price = command.Price,
+                Quantity = command.Quantity,
+                created_at = DateTime.UtcNow,
+                Description = command.Description,
+            };
+            _dbContext.Products.Add(entity);
+            //await _dbContext.SaveChangesAsync(cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return entity.Id;
+        }
+    }
+}
---
+++ ShoppingCart/Controllers/ProductController.cs
@@ -0,0 +1,68 @@
+﻿using Application.Common.Interfaces;
+using Application.Products.Commands.CreateProduct;
+
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShoppingCart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ApiControllerBase
+    {
+        private readonly IApplicationDbContext _dbContext;
+        public ProductController(IApplicationDbContext dbContext) // Dependency Injection is a practice of recreating the instances rather than creating new one everytime. 
+        {
+            this._dbContext = dbContext; 
+        }
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public async Task<int> Post([FromBody] CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            return await Mediator.Send(command, cancellationToken);
+            //CancellationToken cancellationToken = new CancellationToken();
+            //var entity= new Product();
+            //entity.ProductName = product.ProductName;
+            //entity.Description = product.Description;
+            //entity.Price = product.Price;
+            //entity.created_at = DateTime.Now;
+
+           
+            //_dbContext.Products.Add(entity);
+            //await _dbContext.SaveChangesAsync(cancellationToken);
+            
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    
+}
