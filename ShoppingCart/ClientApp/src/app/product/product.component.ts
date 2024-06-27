632985e77ebe4b446a43db1ec58d13a35b5d896d
hemadhami001 <hemadhami365@gamil>
2024-06-27 14:28:43 +0545

    initial

--- ShoppingCart/ClientApp/src/app/product/product.component.ts
+++ ShoppingCart/ClientApp/src/app/product/product.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { Product } from '../Common/Models/product.model';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
   product: Product = new Product();
-constructor(){}
+constructor(private _apiService:ApiService){ }
 
  addProduct(){
-
+this._apiService.addProduct(this.product);
  }
 }
--- ShoppingCart/ClientApp/src/app/services/api.service.ts
+++ ShoppingCart/ClientApp/src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Product } from '../Common/Models/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ApiService {
 
   constructor(private _httpService: HttpClient) { }
 
- // addProduct():Observable<any>{
- //   return this._httpService.post(this.baseUrl+'/api/Product')
- // }
+    addProduct(productDetail:Product):Observable<any>{
+     return this._httpService.post(this.baseUrl+'/api/Product', productDetail);
+  }
 }
